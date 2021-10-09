0d0065ff8c845309b2cdf291218e07e3f07ffb29
xuanhuaz <kaowofeixiang@gmail.com>
2021-10-09 15:53:51 -0400

    resolved merge confict

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register_view(request):
+    if request.method =='GET':
+        return render(request, 'user/SignUp.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password1 = request.POST['password2']
+        password2 = request.POST['password2']
+        if password1 != password2:
+            return HttpResponse('two password must align with eachother')
+
+    user = userInfo.objects.create_user(username=username,password=password1)
+    return HttpResponseRedirect('user/Login.html')
+
+def login_view(request):
+    if request.method == 'GET':
+        return 
+    elif request.method == 'POST':
+        return
+
