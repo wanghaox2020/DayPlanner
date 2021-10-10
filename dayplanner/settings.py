a06af825ccdd513e3f9de7d2631e06c2f95f1ed7
Samuel Ballan <sgb4622@gmail.com>
2021-10-09 20:05:53 -0400

    docker

--- dayplanner/settings.py
+++ dayplanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!74e-5!lwrwbm=z+4fsv5u)@n1%m6su5(l#d5krw6ft*)zpzy$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
