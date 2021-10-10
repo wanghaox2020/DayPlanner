49677129524aa05e062df324407e1459a6856dce
Samuel Ballan <sgb4622@gmail.com>
2021-10-09 20:54:19 -0400

    venues application

---
+++ venues/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Venue(models.Model):
+    yelp_id = models.CharField(max_length=100)
---
+++ venues/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'venues/index.html')
+
+def detail(request):
+    return render(request, 'venues/detail.html')
