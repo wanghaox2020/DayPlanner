b9ebe2d7f58e1d9c218a07f4d5f697cda519c93a
Samuel Ballan <sgb4622@gmail.com>
2021-10-12 18:09:18 -0400

    fix csrf

--- authentication/views.py
+++ authentication/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+# TODO: REMOVE this import.  We SHOULD use csrf
+from django.views.decorators.csrf import csrf_exempt
+
 
 from django.http import HttpResponse
 
 # Create your views here.
 
+# TODO: REMOVE this decorator.  We SHOULD use csrf
+@csrf_exempt
 def register_view(request):
     if request.method =='GET':
         return render(request, 'authentication/signup.html')
@@ -22,6 +27,8 @@ def register_view(request):
     user = User.objects.create_user(username=username,password=password1)
     return HttpResponseRedirect('login')
 
+# TODO: REMOVE this decorator.  We SHOULD use csrf
+@csrf_exempt
 def login_view(request):
     if request.method == 'GET':
         # if this is a get we return a Login page
