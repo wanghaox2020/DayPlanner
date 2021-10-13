53ba8e488e71e78302d3a5d23880a87b63ae215c
yp1383 <yp1383@nyu.edu>
2021-10-12 21:53:52 -0400

    move load_dotenvn to settings

--- dayplanner/settings.py
+++ dayplanner/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 import os
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
