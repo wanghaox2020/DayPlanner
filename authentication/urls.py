0671bcbf3e66d3fc5a7f6bc655841b0b959b6758
yp1383 <yp1383@nyu.edu>
2021-10-17 22:08:17 -0400

    upload user proflie page and enable private method

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ProfileView
 
 urlpatterns = [
     path('signup',views.register_view),
     path('login',views.login_view),
     path('index',views.index_view),
     path('logout',views.logout),
+    path('<int:pk>/',ProfileView.as_view(),)
 ]
