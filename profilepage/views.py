c0a6c2f786d90f1604fcaf0a226656f8c2f43e26
yp1383 <yp1383@nyu.edu>
2021-10-18 13:36:14 -0400

    fix nav bar user to be login user, not profileuser

--- profilepage/views.py
+++ profilepage/views.py
@@ -8,8 +8,9 @@ class ProfileView(DetailView):
     model = Profile
     template_name = 'profilepage/user_profile.html'
     def get_context_data(self, *args, **kwargs):
-        users = Profile.objects.all()
         context = super(ProfileView, self).get_context_data( *args, **kwargs)
+        curr_user = get_object_or_404(Profile, user=self.request.user)
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context['page_user'] = page_user
+        context['curr_user'] = curr_user
         return context
