c9478670662e24b945fbf928212cc0ffb4347004
kevinking <hw2766@nyu.edu>
2021-10-19 15:44:33 -0400

    fetch single venue details

--- resources/venues/models.py
+++ resources/venues/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from dayplanner.services import yelp_client
 
 # Create your models here.
 class Venue(models.Model):
     yelp_id = models.CharField(max_length=100,unique=True)
 
-    @staticmethod
-    def for_yelp_id(yelp_id):
-        venue, created = Venue.objects.get_or_create(yelp_id=yelp_id)
-        return venue if created else False
+    def raw_yelp_data(self):
+        return yelp_client.fetch_by_id(self.yelp_id)
+        
+
