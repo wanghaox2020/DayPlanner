958a5ded894c646c57d2315f6f765fbbaac77610
yp1383 <yp1383@nyu.edu>
2021-10-20 13:20:25 -0400

    add comments

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -3,4 +3,6 @@ from .views import ProfileView
 
 urlpatterns = [
     path('<int:pk>/',ProfileView.as_view(),name='show_profile_page')
+    # ex. lhttp://localhost:8000/profilepage/1/
+    # pk is the primary key in User Model
 ]
