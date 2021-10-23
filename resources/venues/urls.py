cd7610284d622074603f7c33e94f1c3e7921891a
Samuel Ballan <sgb4622@gmail.com>
2021-10-22 21:43:32 -0400

    cleaning up json

--- resources/venues/urls.py
+++ resources/venues/urls.py
@@ -6,4 +6,5 @@ app_name = 'resources.venues'
 urlpatterns = [
     path('', views.index),
     path('<int:venue_id>', views.detail),
+    path('for_yelp_id/<str:yelp_id>', views.for_yelp_id),
 ]
--- resources/venues/views.py
+++ resources/venues/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render, get_object_or_404
 from .models import Venue
 
@@ -14,3 +16,12 @@ def detail(request, venue_id):
         'raw_yelp_data': venue.raw_yelp_data()
     }
     return render(request, 'venues/_detail.html', context)
+
+def for_yelp_id(request, yelp_id):
+     venue, is_created = Venue.objects.get_or_create(yelp_id=yelp_id)
+     data = venue.raw_yelp_data()
+     context = {
+         'dict': data,
+         'json': json.dumps(data, indent=2)
+     }
+     return render(request, 'venues/for_yelp_id.html', context)
