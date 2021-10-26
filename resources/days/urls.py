1871011c9694ec9a821c1eddacfeb98d6c0b4928
Samuel Ballan <sgb4622@gmail.com>
2021-10-25 23:45:19 -0400

    show day venue!

--- resources/days/urls.py
+++ resources/days/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from resources.days.views import DayDetailView
-from resources.days.views import AllDaysView, DayListView
+from resources.days.views import DayDetailView, AllDaysView, DayListView
+from . import views
 
 app_name = "resources.days"
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path("<int:pk>/", DayDetailView.as_view(), name="day_detail"),
     path("", AllDaysView.as_view(), name="all_days"),
     path("<str:username>/", DayListView.as_view(), name="list_of_days"),
+    path("<int:day_id>/edit", views.edit, name="edit"),
     # ex. http://localhost:8000/resources/days/admin/ -> will show day_list admin has
     # ex. http://localhost:8000/resources/days/1
     # -> will show the day detail of the day with primary key "1", in our Day model
