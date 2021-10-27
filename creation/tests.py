6a9acf5bd5daa5c5041d58a64607b7c09ac22eca
xuanhuaz <kaowofeixiang@gmail.com>
2021-10-27 15:31:03 -0400

    added .coveragerc file

--- creation/tests.py
+++ creation/tests.py
@@ -1,3 +1,11 @@
-# from django.test import TestCase
+from django.test import TestCase
 
 # Create your tests here.
+creation_url = "/creation/"
+
+
+class CreationTest(TestCase):
+    def test_creation_page_url(self):
+        response = self.client.get(creation_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "creation/index.html")
