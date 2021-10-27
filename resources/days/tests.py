fe6df44440f9e6eb840ad13741becf0b94b1a0d6
Samuel Ballan <sgb4622@gmail.com>
2021-10-27 16:48:03 -0400

    day edit

--- resources/days/tests.py
+++ resources/days/tests.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
 from django.test import TestCase
 from .models import Day, User
 from django.test import Client
+
+from dayplanner.services.yelp_client import YelpRequest
 
 
 foo_user1 = {
@@ -81,3 +84,27 @@ class DetailViewTest(TestCase):
     def test_set_in_context(self):
         response = self.client.get("/resources/days/1/")
         self.assertEqual(response.context["detail"].name, "test1 DayPlan")
+
+
+class EditViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        user1 = User.objects.create_user(
+            username=foo_user1["username"],
+            email=foo_user1["email"],
+            password=foo_user1["password"],
+            first_name=foo_user1["first_name"],
+            last_name=foo_user1["last_name"],
+        )
+        self.day = Day.objects.create(creator=user1, name="test1 Day")
+
+    def test_set_in_context(self):
+        self.assertEqual(len(self.day.dayvenue_set.all()), 0)
+
+        yelp_data = {"yelp_id": "test_id", "name": "foo", "image_url": "bar"}
+
+        with patch.object(YelpRequest, "execute", return_value=yelp_data):
+            self.client.post(
+                "/resources/days/%i/edit" % self.day.id, {"yelp_id": "test_id"}
+            )
+            self.assertEqual(len(self.day.dayvenue_set.all()), 1)
