9e1286f8cc5a4347cb96024d28f153ef4098de14
yp1383 <yp1383@nyu.edu>
2021-11-07 19:06:47 -0500

    black fix

--- resources/days/migrations/0005_day_description.py
+++ resources/days/migrations/0005_day_description.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('days', '0004_day_is_active'),
+        ("days", "0004_day_is_active"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='day',
-            name='description',
-            field=models.TextField(default='Please Add Description...'),
+            model_name="day",
+            name="description",
+            field=models.TextField(default="Please Add Description..."),
         ),
     ]
