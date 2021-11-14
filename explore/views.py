1115a52e8ae515222f995f007b2fa5f2bd715294
xuanhuaz <kaowofeixiang@gmail.com>
2021-11-13 20:43:46 -0500

    Explore first draft

--- explore/views.py
+++ explore/views.py
@@ -1,6 +1,17 @@
+from django.db import transaction
+from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
+from resources.days.models import Day
+
 
 def explore(requets):
-    return render(requets,"explore/explore.html")
+    context = {}
+    with transaction.atomic():
+        try:
+            days = Day.objects.all()
+            context["days"] = days
+        except Exception as e:
+            return HttpResponse("Error Code: %s" % e)
+
+    return render(requets,"explore/explore.html",context)
