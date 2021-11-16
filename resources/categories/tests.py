d9bdd5122383365bb236e33530613fc427fbcb81
yp1383 <yp1383@nyu.edu>
2021-11-16 15:19:30 -0500

    make flake8 working

--- resources/categories/tests.py
+++ resources/categories/tests.py
@@ -1,5 +1,4 @@
 # Create your tests here.
-from unittest.mock import patch
 from django.test import TestCase
 from .models import Day
 from django.contrib.auth.models import User
@@ -26,16 +25,18 @@ class CategoryModelTest(TestCase):
             first_name=foo_user1["first_name"],
             last_name=foo_user1["last_name"],
         )
-        day1 = Day.objects.create(creator=user1, name="test1 DayPlan1")
+        Day.objects.create(creator=user1, name="test1 DayPlan1")
 
     def add_new_category(self):
-        cat1 = Category.objects.create(cat="sushi")
+        Category.objects.create(cat="sushi")
         self.assertTrue(len(Category.objects.get_queryset()) == 1)
 
 
 class DayCategoryModelTest(TestCase):
     def setUp(self):
         self.client = Client()
+
+    def add_daycategory(self):
         user1 = User.objects.create_user(
             username=foo_user1["username"],
             email=foo_user1["email"],
@@ -45,9 +46,5 @@ class DayCategoryModelTest(TestCase):
         )
         day1 = Day.objects.create(creator=user1, name="test1 DayPlan1")
         cat1 = Category.objects.create(cat="sushi")
-
-    def add_daycategory(self):
-        DayCategory.objects.create(day=self.day1, cat=self.cat1)
-        self.assertTrue(
-            len(DayCategory.objects.filter(day=self.day1, cat=self.cat1)) == 1
-        )
+        DayCategory.objects.create(day=day1, cat=cat1)
+        self.assertTrue(len(DayCategory.objects.filter(day=day1, cat=cat1)) == 1)
