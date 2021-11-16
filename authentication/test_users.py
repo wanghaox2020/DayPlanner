fddca6b2d169b1cd27a8b06aea12506c7ef3a263
Samuel Ballan <sgb4622@gmail.com>
2021-11-16 11:37:08 -0500

    safely create test users with a transaction and check

--- authentication/test_users.py
+++ authentication/test_users.py
@@ -1,26 +1,23 @@
 def create_test_users():
     from django.contrib.auth import get_user_model
+    from django.db import transaction
 
     User = get_user_model()
 
-    User.objects.get_or_create(
-        email="prof@mailinator.com", username="prof", password="asdf"
-    )
-    User.objects.get_or_create(
-        email="ta@mailinator.com", username="ta", password="asdf"
-    )
-    User.objects.get_or_create(
-        email="test1@mailinator.com", username="test1", password="asdf"
-    )
-    User.objects.get_or_create(
-        email="test2@mailinator.com", username="test2", password="asdf"
-    )
-    User.objects.get_or_create(
-        email="test3@mailinator.com", username="test3", password="asdf"
-    )
-    User.objects.get_or_create(
-        email="test4@mailinator.com", username="test4", password="asdf"
-    )
-    User.objects.get_or_create(
-        email="test5@mailinator.com", username="test5", password="asdf"
-    )
+    usernames = ["prof", "ta", "test1", "test2", "test3", "test4", "test5"]
+
+    for username in usernames:
+        try:
+            with transaction.atomic():
+                existing_user = User.objects.filter(username=username).first()
+                if existing_user:
+                    continue
+
+                User.objects.create_user(
+                    email="%s@mailinator.com" % username,
+                    username=username,
+                    password="asdf",
+                )
+        except Exception as e:
+            print("Encountered Error while creating test users:\n%s" % e)
+            break
