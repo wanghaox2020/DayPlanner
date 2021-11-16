0db44f4c506962b598f9412bc7ec803522f3736e
xuanhuaz <kaowofeixiang@gmail.com>
2021-11-16 16:06:47 -0500

    Added Exploration test 1

--- explore/tests.py
+++ explore/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from resources.days.models import Day
 
 # Create your tests here.
+
+class TestExplore(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.explore_url = "/explore/"
+
+    def test_explore_index(self):
+        response = self.client.get(self.explore_url)
+        self.assertEqual(response.status_code, 200)
+
+
+
+
