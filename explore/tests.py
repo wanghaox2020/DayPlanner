de98749b7d4b8683319371baad68a6a81bbc47d9
xuanhuaz <kaowofeixiang@gmail.com>
2021-11-22 13:19:09 -0500

    flake 8

--- explore/tests.py
+++ explore/tests.py
@@ -11,8 +11,8 @@ class TestExplore(TestCase):
     def setUp(self):
         self.client = Client()
         self.explore_url = "/explore/"
-        self.search_empty = {"search_input":""}
-        self.search_test = {"search_input":"test"}
+        self.search_empty = {"search_input": ""}
+        self.search_test = {"search_input": "test"}
         User = get_user_model()
         self.test_username = "test"
         self.test_password = "test"
@@ -27,7 +27,6 @@ class TestExplore(TestCase):
         self.test_cat = Category.objects.create(cat="TestCat")
         DayCategory.objects.create(day=self.test_day, cat=self.test_cat)
 
-
     def test_explore_index(self):
         response = self.client.get(self.explore_url)
         self.assertEqual(response.status_code, 200)
@@ -41,18 +40,18 @@ class TestExplore(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_explore_day_searchEmpty(self):
-        response = self.client.post("/explore/search",self.search_empty)
+        response = self.client.post("/explore/search", self.search_empty)
         self.assertEqual(response.status_code, 200)
 
     def test_explore_day_search(self):
-        response = self.client.post("/explore/search",self.search_test)
+        response = self.client.post("/explore/search", self.search_test)
         self.assertEqual(response.status_code, 200)
 
     def test_explore_tags(self):
-        response = self.client.get("/explore/"+self.test_cat.cat)
+        response = self.client.get("/explore/" + self.test_cat.cat)
         self.assertEqual(response.status_code, 200)
 
     def test_fork(self):
         self.client.login(username=self.test_username, password=self.test_password)
-        response = self.client.get("/explore/"+str(self.test_day.id) + "/fork")
+        response = self.client.get("/explore/" + str(self.test_day.id) + "/fork")
         self.assertEqual(response.status_code, 302)
