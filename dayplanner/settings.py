9b109efec9d56241b9c8b92e8d63933bb35b5eb2
Samuel Ballan <sgb4622@gmail.com>
2021-12-06 21:20:59 -0500

    fix whitenoise debug

--- dayplanner/settings.py
+++ dayplanner/settings.py
@@ -170,7 +170,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "dayplanner/static"),)
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
