0b092f950249ce1b54b413d43fed4540841ca9b9
xuanhuaz <kaowofeixiang@gmail.com>
2021-12-07 12:57:18 -0500

    #197 Requirement4 completed

--- authentication/views.py
+++ authentication/views.py
@@ -12,22 +12,26 @@ from django.views.decorators.csrf import csrf_exempt
 # TODO: REMOVE this decorator.  We SHOULD use csrf
 @csrf_exempt
 def register_view(request):
+    context = {}
+    handle_message(request, context)
     if request.method == "GET":
-        return render(request, "authentication/signup.html")
+        return render(request, "authentication/signup.html", context)
     elif request.method == "POST":
         email = request.POST["email"]
         username = request.POST["username"]
         password1 = request.POST["password1"]
         password2 = request.POST["password2"]
         if password1 != password2:
-            return HttpResponse("two password must align with eachother")
+            request.session["Error_Message"] = "Password and Repeat Password should be the same!"
+            return HttpResponseRedirect(request.path)
         try:
             user = User.objects.create_user(
                 email=email, username=username, password=password1
             )
             login(request, user)
         except Exception as e:
-            return HttpResponse("Error Code: %s" % e)
+            request.session["Error_Message"] = str(e)
+            return HttpResponseRedirect(request.path)
 
     return HttpResponseRedirect("index")
 
@@ -35,16 +39,20 @@ def register_view(request):
 # TODO: REMOVE this decorator.  We SHOULD use csrf
 @csrf_exempt
 def login_view(request):
+    context = {}
+    handle_message(request, context)
     if request.method == "GET":
         # if this is a get we return a Login page
-        return render(request, "authentication/login.html")
+        return render(request, "authentication/login.html", context)
     elif request.method == "POST":
         user = request.POST["username"]
         pwd = request.POST["password"]
 
         user = authenticate(username=user, password=pwd)
         if not user:
-            return HttpResponse("Login failed")
+            # Create Error Message
+            request.session["Error_Message"] = "Your Username/Password is incorrect !!"
+            return HttpResponseRedirect(request.path)
         else:
             login(request, user)
             if request.user.is_superuser:
@@ -62,3 +70,12 @@ def logout(request):
 def index_view(request):
     login_user = request.user
     return render(request, "dayplanner/index.html", locals())
+
+
+def handle_message(request, context):
+    if "Error_Message" in request.session:
+        context["error"] = request.session["Error_Message"]
+        del request.session["Error_Message"]
+    elif "Success_Message" in request.session:
+        context["message"] = request.session["Success_Message"]
+        del request.session["Success_Message"]
