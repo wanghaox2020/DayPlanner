1fd212965de0986100d3c075c98fe7616a65ec90
xuanhuaz <kaowofeixiang@gmail.com>
2021-12-07 12:35:29 -0500

    #197 Requirement3 completed

--- explore/views.py
+++ explore/views.py
@@ -66,13 +66,17 @@ def explore_cats(requests, cat):
 
 def day_summary(requests, day_id):
     day = get_object_or_404(Day, pk=day_id)
+    if day.creator == requests.user:
+        return HttpResponseRedirect("/creation/%s/detail" % day.id)
     context = {}
     context["day"] = day
     context["active_categories"] = day.daycategory_set.all()
     DayVenues = day.dayvenue_set.all()
     fetch_list = []
     dayvenue_list = []
+    # if the user is the one who created it, redirect to the creation page
 
+    # Handle the message
     handle_message(requests, context)
 
     for dv in DayVenues:
